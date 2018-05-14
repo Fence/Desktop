9b1eb176d7ff27c27f63907bc03ae707c9f949d2
Fence <787499313@qq.com>
2018-05-14 22:57:34 +0800

    rm DistantDQN

--- DPR_ALL/DistantDQN/testpipeline.sh
+++
@@ -1,7 +0,0 @@
-for db in wikihow
-do
-    python main.py \
-    --agent_mode 'multi' \
-    --actionDB $db \
-    --result_dir 'pipeline_result'
-done
--- DPR_ALL/DistantDQN/trainmulti.sh
+++
@@ -1,9 +0,0 @@
-python main.py \
---epochs 3 \
---display_training_result 1 \
---agent_mode multi \
---test_one_flag 0 \
---gpu_rate 0.24 \
---load_weights "weights" \
---save_weights_prefix "weights" \
---result_dir "results/multi/training/result" 
